5d00af7d0d56ab29f29a0bc76b9d2fd2fcbe3996
pi224 <zhang.daniel9797@gmail.com>
2018-06-03 21:08:14 -0400

    fixed untracked files

--- config.py
+++
@@ -1,4 +0,0 @@
-OUTFILENAMEBASE = 'loop_test_out'
-OUTFILETYPE = 'FULL.png'
-DATADIR = '/scratch/tewaria_fluxm/dtzhang/Young_sandbox/BanditSLC/data'
-RESULTSDIR = '/scratch/tewaria_fluxm/dtzhang/Young_sandbox/BanditSLC/resultsMK2/'
